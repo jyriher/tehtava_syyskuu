19d5ca3f5a28b7f59f2930e7a33de0a50db8ed3b
jyriher <jyriher@metropolia.fi>
2018-09-21 22:37:28 +0300

    21 syyskuuta

--- tehtava.js
+++ tehtava.js
@@ -1,8 +1,11 @@
 const one = document.querySelector('#one');
 const two = document.querySelector('#two');
 const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
 const title1 = document.querySelector('#title1');
 const title2 = document.querySelector('#title2');
+const titleGrey = document.querySelector('#titleGrey');
 
 const greenbox1text = document.querySelector('#greenbox1text');
 const bluebox1text = document.querySelector('#bluebox1text');
@@ -13,6 +16,10 @@ const orangeboxtext = document.querySelector('#orangeboxtext');
 const proxyosoite = 'https://cors-anywhere.herokuapp.com/'
 const arrowButton = document.querySelector('#arrowButton');
 const container = document.querySelector('#container');
+const leftInfo = document.querySelector('#leftInfo');
+const leftColumn = document.querySelector('#leftColumn');
+const leftColumn2 = document.querySelector('#leftColumn2');
+const leftColumn3 = document.querySelector('#leftColumn3');
 
 const getText = () => {
 
@@ -47,6 +54,26 @@ const getText = () => {
 
 arrowButton.addEventListener('click', () => {
     info2.style.display = 'none';
+    arrowButton.style.display = 'none';
+    three.style.display = 'none';
+   // four.style.width = '36%';
+    leftInfo.style.display = 'flex';
+    leftColumn.style.display = 'block';
+    leftColumn2.style.display = 'block';
+    leftColumn3.style.display = 'block';
+ //   leftColumn2.style.background = 'blue';
+    info.style.width = '56em';
+    
+    fetch(proxyosoite + 'http://users.metropolia.fi/~jyriher/tehtava_paivitys.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((result) => {
+        four.innerHTML = result.instructions.help[0];
+        five.innerHTML = result.instructions.help[1];
+        titleGrey.innerHTML = result.instructions.chart;
+       // console.log(result.instructions.help[0]);
+    });
 });
 
 getText();
